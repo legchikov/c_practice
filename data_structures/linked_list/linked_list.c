d7c10d8a07d8c2445e66ef7951816a1df03cfdd6
Dmitry Legchikov <legchikov@dmitry.by>
2017-01-02 17:13:38 +0300

    add book

--- data_structures/linked_list/linked_list.c
+++ data_structures/linked_list/linked_list.c
@@ -1,11 +1,57 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+void append(int data);
+void display();
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+struct node *next = NULL;
+
 int main(){
     cout << "Hello Linked List" << endl;
+    append(1);
+    append(2);
+    append(5);
+    display();
     
     cin.clear();
     cin.get();
     return 0;    
+}
+
+void display(){
+    struct node *ptr = head;
+    cout << "\n";
+
+    while(ptr != NULL){
+        cout << "[" <<ptr->data << "] --> ";
+        ptr = ptr->next;
+    }
+
+    cout << "[NULL]";
+}
+
+void append(int data){
+    struct node *link = (struct node*) malloc(sizeof(struct node));
+    link->data = data;
+    link->next = NULL;
+
+    if(head == NULL){
+        head = link;
+        return;
+    }
+    
+    struct node *current = head;
+
+    while(current->next != NULL){
+        current = current->next;
+    }
+    current->next = link;
 }
