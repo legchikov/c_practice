5b53b95454c7db275ff3c2879fc12672fc5574d1
Dmitry Legchikov <legchikov@dmitry.by>
2017-01-29 14:54:20 +0300

    add compilation

---
+++ compilation/main.c
@@ -0,0 +1,7 @@
+#include "square.h"
+
+int main(){
+  square(5);
+
+  return 0;
+}
---
+++ compilation/main_preprocessed.c
@@ -0,0 +1,18 @@
+# 1 "main.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 1 "<command-line>" 2
+# 1 "main.c"
+# 1 "square.h" 1
+
+
+
+int square(int x);
+# 2 "main.c" 2
+
+int main(){
+  square(5);
+
+  return 0;
+}
