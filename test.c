ee4e3ab6bdb6305a399ae9fce572f68ec29ca647
Dmitry Legchikov <legchikov@dmitry.by>
2017-02-15 23:48:13 +0300

    add

--- test.c
+++ test.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
+#include "math.h"
 int main(int argc, char const *argv[]) {
     /* unspecified behavior */
-    int i = 10;
+    /*int i = 10;
     i = (i += 5) + (i * 10);
     printf("%d\n", i);
+*/
 
+    printf("%.5s = %.5f", "value azaza", M_PI
+);
     return 0;
 }
